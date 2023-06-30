bd4c4b521fc19500f2606a6ce7995df19f1e43d9
Gene Browder <genebrowder@gmail.com>
2023-06-30 03:00:23 -0500

    Minor changes

--- src/com/coderscampus/service/CsvReaderService.java
+++ src/com/coderscampus/service/CsvReaderService.java
@@ -9,44 +9,38 @@ import java.util.List;
 import com.coderscampus.domain.Product;
 
 public class CsvReaderService {
-	
-	public CsvReaderService() {
 
-	}
-	
-	public List<Product> readProductsFromFile(Path path)  {
-		
-		List<Product> productList = new ArrayList<>();
-			
-		try {
-			
-			List<String> productListFromFile = Files.readAllLines(path);
-			
+    public CsvReaderService() {
 
-			productListFromFile.stream()
-			    .skip(1)
-			    .forEach((String lineOfData) -> {
-				
-				Product product = new Product();
-				
-				String[] aParcedProductRowArrayOfString = lineOfData.split(",");
-				
-				product.setId(Integer.parseInt(aParcedProductRowArrayOfString[0]));
-				product.setName(aParcedProductRowArrayOfString[1]);
-				product.setQuantity(Integer.parseInt(aParcedProductRowArrayOfString[2]));
-				product.setPrice(Double.parseDouble(aParcedProductRowArrayOfString[3]));
-				
+    }
 
-				productList.add(product);
-			});
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println("There was a problem reading the file");
-			e.printStackTrace();
-		}
-		
-		return productList;
-	}
+    public List<Product> readProductsFromFile(Path path) {
 
+        List<Product> productList = new ArrayList<>();
+
+        try {
+            List<String> productListFromFile = Files.readAllLines(path);
+
+            productListFromFile.stream()
+                    .skip(1)
+                    .forEach((String lineOfData) -> {
+
+                        Product product = new Product();
+
+                        String[] aParcedProductRowArrayOfString = lineOfData.split(",");
+
+                        product.setId(Integer.parseInt(aParcedProductRowArrayOfString[0]));
+                        product.setName(aParcedProductRowArrayOfString[1]);
+                        product.setQuantity(Integer.parseInt(aParcedProductRowArrayOfString[2]));
+                        product.setPrice(Double.parseDouble(aParcedProductRowArrayOfString[3]));
+
+                        productList.add(product);
+                    });
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            System.out.println("There was a problem reading the file");
+            e.printStackTrace();
+        }
+        return productList;
+    }
 }
